1807120a368de26c8290bcea0eb9493e357f64fa
steveoc <steveoc64@gmail.com>
2019-01-06 12:02:18 +1000

    add kontrol vars to the startup script

--- kontroller.sh
+++ kontroller.sh
@@ -1,9 +1,18 @@
 #! /bin/sh
-cd /root
+
+export KONTROL_PORT=4099
+export KONTROL_USERNAME="kontrol"
+export KONTROL_STORAGE="etcd"
+export KONTROL_KONTROLURL="http://10.240.13.11:4099/kite"
+export KONTROL_PUBLICKEYFILE=/root/certs/key_pub.pem
+export KONTROL_PRIVATEKEYFILE=/root/certs/key.pem
+ 
 echo starting etcd
 daemon -o /root/etcd.log /usr/local/bin/etcd &
 echo .. waiting for startup
-sleep 2
+sleep 1
 echo starting kontrol
-daemon -o /root/kontrol.log /root/go/bin/kontrol &
+daemon -o /root/kontrol.log /root/go/bin/kontrol
+~
+
 
